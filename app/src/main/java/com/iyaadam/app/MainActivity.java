f15c192d3f40eb74d58c33259539b89956acd589
FulgentDigit <nwagbarakelechi15@gmail.com>
2026-09-12 00:18:27 +0100

    Update sample dishes with new entries and images

--- app/src/main/java/com/iyaadam/app/MainActivity.java
+++ app/src/main/java/com/iyaadam/app/MainActivity.java
@@ -64,29 +64,29 @@ public class MainActivity extends AppCompatActivity implements DishAdapter.OnDis
     private List<Dish> createSampleDishes() {
         List<Dish> dishList = new ArrayList<>();
         
-        dishList.add(new Dish("1", "Jollof Rice Special", 2500, "Aromatic rice with spices", 
-            "https://images.unsplash.com/photo-1546069901-ba9599a7e63c?w=400", 4.7, 152, 20, true));
+        dishList.add(new Dish("1", "Fufu & Ewedu", 2500, "Traditional African delicacy with rich soup", 
+            "images_image", 4.7, 152, 20, true));
         
-        dishList.add(new Dish("2", "Grilled Chicken", 3000, "Perfectly seasoned chicken breast", 
-            "https://images.unsplash.com/photo-1598103442097-8b74394b95c6?w=400", 4.8, 198, 25, true));
+        dishList.add(new Dish("2", "Jollof Rice & Chicken", 3000, "Classic Nigerian taste at its best", 
+            "images8", 4.8, 198, 25, true));
         
         dishList.add(new Dish("3", "Goat Meat Pepper Sauce", 3500, "Tender goat in spicy sauce", 
-            "https://images.unsplash.com/photo-1626082927389-6cd097cfd330?w=400", 4.6, 87, 30, true));
+            "goat_meat_pepper_soup", 4.6, 87, 30, true));
         
-        dishList.add(new Dish("4", "Suya Beef", 2800, "Spiced roasted beef strips", 
-            "https://images.unsplash.com/photo-1555939594-58d7cb561404?w=400", 4.5, 124, 15, true));
-        
+        dishList.add(new Dish("4", "Creamy Pasta & Chicken", 3500, "Rich, smooth and satisfying", 
+            "images234", 4.5, 124, 15, true));
+
         dishList.add(new Dish("5", "Moi Moi", 1500, "Steamed bean pudding", 
-            "https://images.unsplash.com/photo-1609501676725-7186f017a4b5?w=400", 4.4, 76, 20, true));
+            "images10", 4.4, 76, 20, true));
         
         dishList.add(new Dish("6", "Pepper Soup", 2000, "Hot & flavorful broth", 
-            "https://images.unsplash.com/photo-1547592166-23ac45744acd?w=400", 4.3, 95, 15, true));
+            "images11", 4.3, 95, 15, true));
         
         dishList.add(new Dish("7", "Pounded Yam with Egusi", 4000, "Smooth yam with egusi gravy", 
-            "https://images.unsplash.com/photo-1596103442097-8b74394b95c6?w=400", 4.9, 213, 25, true));
+            "images13", 4.9, 213, 25, true));
         
-        dishList.add(new Dish("8", "Tuwo de Rice", 2200, "Creamy savory rice meal", 
-            "https://images.unsplash.com/photo-1546069901-ba9599a7e63c?w=400", 4.2, 64, 18, true));
+        dishList.add(new Dish("8", "Rice & Vegetable", 2200, "Creamy savory rice meal", 
+            "images15", 4.2, 64, 18, true));
 
         return dishList;
     }
