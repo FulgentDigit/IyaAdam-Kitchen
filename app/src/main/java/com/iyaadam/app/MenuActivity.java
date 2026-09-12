8b37685aab65ab159fd7251b15a7d4285536454e
FulgentDigit <nwagbarakelechi15@gmail.com>
2026-09-12 17:38:31 +0100

    Update MenuActivity.java

--- app/src/main/java/com/iyaadam/app/MenuActivity.java
+++ app/src/main/java/com/iyaadam/app/MenuActivity.java
@@ -16,32 +16,36 @@ public class MenuActivity extends AppCompatActivity {
 
     private RecyclerView menuRecycler;
     private DishAdapter adapter;
-    private ImageView backBtn;
-    private TextView customOrderBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
-        backBtn = findViewById(R.id.back_btn);
-        customOrderBtn = findViewById(R.id.custom_order_btn);
+        ImageView backBtn = findViewById(R.id.back_btn);
+        TextView customOrderBtn = findViewById(R.id.custom_order_btn);
+
         menuRecycler = findViewById(R.id.menu_recycler);
 
         backBtn.setOnClickListener(v -> finish());
 
         customOrderBtn.setOnClickListener(v -> {
-            Intent intent = new Intent(MenuActivity.this, CustomOrderActivity.class);
+            Intent intent = new Intent(
+                    MenuActivity.this,
+                    CustomOrderActivity.class
+            );
             startActivity(intent);
         });
 
-        menuRecycler.setLayoutManager(new LinearLayoutManager(this));
+        menuRecycler.setLayoutManager(
+                new LinearLayoutManager(this)
+        );
 
-        List<Dish> allDishes = createAllDishes();
+        List<Dish> dishes = createAllDishes();
 
         adapter = new DishAdapter(
                 this,
-                allDishes,
+                dishes,
                 new DishAdapter.OnDishClickListener() {
 
                     @Override
@@ -63,125 +67,37 @@ public class MenuActivity extends AppCompatActivity {
 
         List<Dish> dishes = new ArrayList<>();
 
-        dishes.add(new Dish(
-                "1",
-                "Fufu & Ewedu",
-                2500,
-                "Traditional leafy soup",
-                "images",
-                4.7,
-                152,
-                20,
-                true
-        ));
+        dishes.add(new Dish("1", "Fufu & Ewedu",
+                2500, "Traditional leafy soup",
+                "images", 4.7, 152, 20, true));
 
-        dishes.add(new Dish(
-                "2",
-                "Jollof Rice & Chicken",
-                3000,
-                "Spiced rice perfection",
-                "images8",
-                4.8,
-                198,
-                25,
-                true
-        ));
+        dishes.add(new Dish("2", "Jollof Rice & Chicken",
+                3000, "Spiced rice perfection",
+                "images8", 4.8, 198, 25, true));
 
-        dishes.add(new Dish(
-                "3",
-                "Goat Meat Pepper Sauce",
-                3500,
-                "Tender meat in spicy broth",
-                "images13",
-                4.6,
-                87,
-                30,
-                true
-        ));
+        dishes.add(new Dish("3", "Goat Meat Pepper Sauce",
+                3500, "Tender meat in spicy broth",
+                "images13", 4.6, 87, 30, true));
 
-        dishes.add(new Dish(
-                "4",
-                "Creamy Pasta & Chicken",
-                3500,
-                "Italian meets African",
-                "images234",
-                4.5,
-                124,
-                15,
-                true
-        ));
+        dishes.add(new Dish("4", "Creamy Pasta & Chicken",
+                3500, "Italian meets African",
+                "images234", 4.5, 124, 15, true));
 
-        dishes.add(new Dish(
-                "5",
-                "Moi Moi",
-                1500,
-                "Steamed bean pudding",
-                "images10",
-                4.4,
-                76,
-                20,
-                true
-        ));
+        dishes.add(new Dish("5", "Moi Moi",
+                1500, "Steamed bean pudding",
+                "images10", 4.4, 76, 20, true));
 
-        dishes.add(new Dish(
-                "6",
-                "Pepper Soup",
-                2000,
-                "Hot traditional broth",
-                "images11",
-                4.3,
-                95,
-                15,
-                true
-        ));
+        dishes.add(new Dish("6", "Pepper Soup",
+                2000, "Hot traditional broth",
+                "images11", 4.3, 95, 15, true));
 
-        dishes.add(new Dish(
-                "7",
-                "Pounded Yam & Egusi",
-                4000,
-                "Smooth yam with soup",
-                "images13",
-                4.9,
-                213,
-                25,
-                true
-        ));
+        dishes.add(new Dish("7", "Pounded Yam & Egusi",
+                4000, "Smooth yam with soup",
+                "images13", 4.9, 213, 25, true));
 
-        dishes.add(new Dish(
-                "8",
-                "Rice & Vegetable",
-                2200,
-                "Creamy mixed vegetables",
-                "images15",
-                4.2,
-                64,
-                18,
-                true
-        ));
-
-        dishes.add(new Dish(
-                "9",
-                "Amala & Ewedu",
-                2800,
-                "Cornmeal with leafy soup",
-                "amala_with_ewedu_and_goat_meat",
-                4.6,
-                141,
-                22,
-                true
-        ));
-
-        dishes.add(new Dish(
-                "10",
-                "Egusi Soup",
-                2200,
-                "Melon seed delicacy",
-                "egusi_soup",
-                4.5,
-                108,
-                18,
-                true
-        ));
+        dishes.add(new Dish("8", "Rice & Vegetable",
+                2200, "Creamy mixed vegetables",
+                "images15", 4.2, 64, 18, true));
 
         return dishes;
     }
